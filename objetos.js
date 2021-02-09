ce7f0c8d4f329080f437d7f35407680f690685a1
Matheus de Oliveira <71405941+matheusoliverr@users.noreply.github.com>
2021-02-09 16:00:25 -0300

    Adiciona descrição

--- objetos.js
+++ objetos.js
@@ -1,5 +1,4 @@
-//empresa: nome rocketseat, cor roxo, foco programação, endereço: rua guilherme gembala numero 260
-//A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260
+//Escrever mensagem no terminal utilizando objetos e variáveis
 
 const empresa={
     nome: "Rocketseat",
@@ -13,10 +12,6 @@ const empresa={
 
 console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}`)
 
-
-//programador: nome, idade, tecnologias: nome e especialidade
-//nome:C++, especialidade: Desktop; nome:Python, especialidade:Data Science; nome:JavaScript, especialidade:Web/Mobile
-//O usuário Carlos tem 32 anos e usa a tecnologia C++ com especialidade em Desktop
 
 const programador={
     nome:"Carlos",
